e9c59da342cd66d4809914140f30c62a537dd53b
chi-nhan.vo@fullsix.com <Chi-Nhan Vo>
2016-12-15 12:11:15 +0700

    fix lang key to get correct package lang

--- src/RandomQuote.php
+++ src/RandomQuote.php
@@ -2,12 +2,14 @@
 
 class RandomQuote {
 
+    protected $langKey = 'ronnyvo/randomquote::randomquote.quotes';
+
     public function get($key) {
-        return $quotes = trans('randomquote.quotes')[$key];
+        return $quotes = trans($this->langKey)[$key];
     }
 
     public function getRandom() {
-        $quotes = trans('randomquote.quotes');
+        $quotes = trans($this->langKey);
         return $quotes[array_rand($quotes)];
     }
     
